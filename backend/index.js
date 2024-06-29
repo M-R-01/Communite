1500fe3e01f7845e812a1c58a512af757a144207
M-R-0-1 <ramiz01012005@gmail.com>
2024-06-29 13:51:55 +0530

    Second commit

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { PORT } from './config.js';
 import mysql from 'mysql';
 import cors from 'cors';
+import bcrypt from 'bcrypt';
 
 const app = express();
 
@@ -20,5 +21,31 @@ app.listen(PORT, (err) => {
     console.log(`Server is running on port ${PORT}`);
 });
 
+const saltRounds = 10;
+
+app.post('/signup', (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const mobile = req.body.mobile;
+    const age = req.body.age;
+    const address = req.body.address;
+    const password = req.body.password;
+    const pronoun = req.body.pronoun;
+
+    console.log(name, email, mobile, age, address, password, pronoun);
+
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+        if (err) {
+            console.log(err);
+        } else {
+            db.query('INSERT INTO users (name, email, mobile, Age, Address, Password, pronouns) VALUES (?,?,?,?,?,?,?)',
+                [name, email, mobile, age, address, hash, pronoun], (err, result) => {
+                if (err) console.log(err);
+                res.status(200).send('User ' + name + 'created successfully');
+                console.log(res);
+            })
+        }
+    })
+})
 
 
--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -1,16 +1,79 @@
 import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
 const Signup = () => {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [mobile, setMobile] = useState('');
+    const [age, setAge] = useState('');
+    const [address, setAddress] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [pronoun, setPronoun] = useState('');
+
+    const handleSignup = async (e) => {
+        e.preventDefault();
+
+        if (name == '' || email == '' || age == '' || address == '' || password == '' || confirmPassword == '' || pronoun == '') {
+            alert('Please fill in all fields');
+            return;
+        }
+
+        if (password.length < 8) {
+            alert('Password must be at least 8 characters');
+            return;
+        }
+
+        if (!email.includes('@')) {
+            alert('Invalid email');
+            return;
+        }
+
+        if (mobile.length >10) {
+            alert('Invalid mobile number');
+            return;
+        }
+
+        if (password != confirmPassword) {
+            alert('Passwords do not match');
+            return;
+        }
+
+        const data = {
+            name: name,
+            email: email,
+            mobile: mobile,
+            age: age,
+            address: address,
+            password: password,
+            pronoun: pronoun,
+        }
+
+        axios.post('http://localhost:3000/signup', data)
+          .then((res) => {
+            if (res.data) {
+                alert('Signup successful');
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+        };
+
     return (
         <div>
             <form>
-                <input type="text" placeholder="Name" />
-                <input type="text" placeholder="Email" />
-                <input type='number' placeholder="Age" />
-                <textarea placeholder="Address" />
-                <input type="text" placeholder="Password" />
-                <input type="text" placeholder="Confirm Password" />
-                <button type="submit">Sign Up</button>
+                <input type="text" placeholder="Name" onChange={(e) => (setName(e.target.value))} required />
+                <input type="text" placeholder="Email" onChange={(e) => (setEmail(e.target.value))} required />
+                <input type='text' placeholder="Mobile" onChange={(e) => (setMobile(e.target.value))} required />
+                <input type='number' placeholder="Age" onChange={(e) => (setAge(e.target.value))} required />
+                <textarea placeholder="Address" onChange={(e) => (setAddress(e.target.value))} required />
+                <input type="password" placeholder="Password" onChange={(e) => (setPassword(e.target.value))} required />
+                <input type="password" placeholder="Confirm Password" onChange={(e) => (setConfirmPassword(e.target.value))} required />
+                <input type="text" placeholder="Pronouns" onChange={(e) => (setPronoun(e.target.value))} required />
+                <input type='submit' placeholder="Sign Up" onChange={handleSignup} />
             </form>
         </div>
     );
